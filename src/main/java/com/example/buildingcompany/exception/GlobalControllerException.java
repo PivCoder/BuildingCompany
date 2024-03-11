10dcb6260ea1e82087b33ec19bf15740e204bd5e
Илья <ilia10d2020@gmail.com>
2024-03-11 17:47:09 +0400

    Яковенко Илья: Написал шлобальный обработчик ошибок. Требуеться проверка.

--- src/main/java/com/example/buildingcompany/exception/GlobalControllerException.java
+++ src/main/java/com/example/buildingcompany/exception/GlobalControllerException.java
@@ -1,5 +1,18 @@
 package com.example.buildingcompany.exception;
 
-/*TODO написать простой контроллер исключений*/
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalControllerException {
+
+    @ExceptionHandler(ElementNotFoundException.class)
+    public ResponseEntity<String> handleElementNotFoundException(ElementNotFoundException ex)
+    {
+        // Log the exception here if desired
+        String errorMessage = "Запрашиваемый элемент не найден:" + ex.getMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+    }
 }
